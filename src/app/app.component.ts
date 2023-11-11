55d04c76c9f228d6da61b2afaf8b5d1d93443391
jkhanji <jkhanji@turning.com>
2023-11-11 08:39:40 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Post } from './post';
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule, CardComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'project';
+  url ='https://jsonplaceholder.typicode.com/posts';
+  postList: Array<Post>;
+  constructor(
+    private httpClient: HttpClient
+  ) {
+    console.log('test1');
+    this.httpClient.get(this.url).subscribe((results: Array<Post>) => {
+      this.postList = results;
+      console.log(this.postList);
+    });
+    console.log('test2')
+  }
 }
